baa3ad75baea5dc4f55b28e16b08a86daed7f17d
Ballyev <ballyev06@gmail.com>
2024-10-19 09:47:44 +0000

    main.cpp

--- Assignment2b/main.cpp
+++ Assignment2b/main.cpp
@@ -1,8 +1,7 @@
-#include<iostream>
-#include<string>
-#include<cstdlib>
-#include<cctype>
-#include<stdexcept>
+//Ballyyev Eziz st135568@student.spbu.ru
+#include <iostream>
+#include <string>
+#include <cstdlib>
 
 int main()
 {
@@ -11,78 +10,63 @@ int main()
   int top = -1;
 
   std::string input;
-  std::cout<< "Enter input in Reverse Polish Notation:";
-  std::getline(std::cin,input);
+  std::cout << "Error input in Reverse Polish Notation: ";
+  std::getline(std::cin, input);
 
   std::string token;
 
-  for (size_t i=0; i < input.size(); ++i)
-  { 
+  for (size_t i = 0; i < input.size(); ++i)
+  {
     char c = input[i];
 
-    if(c ==' ')continue;
+    if (c ==' ') continue;
 
-    if(c == '+' || c == '-' || c == '*' || c == '/')
+    if (c == '+' || c == '-' || c == '+' || c == '/')
     {
-      if(top < 1)
+      if (top < 1)
       {
-        std::cout<< "not enough operands for operation" << c << std::endl;
+        std::cout << "Not enough operand for operation " << c << std::endl;
         delete[] stack;
         return 0;
       }
-       
+
       double b = stack[top--];
       double a = stack[top--];
 
       double result = 0;
       switch (c)
       {
-        case '+':result= a + b;break;
-        case '-':result= a - b;break;
-        case '*':result= a * b;break;
-        case '/':if (b == 0)
-        {
-          std::cout<<"division by zero error"<<std::endl;
-          delete[] stack;
-          result; 0;
-        }
-        result = a/b;
-        break;
-     
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/': result = a / b; break;
       }
-      
-      stack[++top]=result;
+
+      stack[++top] = result;
     }
-    else if(isdigit(c) || (c =='-'&& i+1 < input.size()&&isdigit(input[i + 1])))
+    else if (isdigit(c) || c == '-')
     {
       token.clear();
-      while(i<input.size()&&(isdigit(input[i] || input[i]=='.')))
-    {
-      token += input[i];
-      ++i;
+      while (i < input.size() && (isdigit(input[i]) || input[i] == '.' || input[i] == '-'))
+      {
+        token += input[i];
+        ++i;
+      }
+      --i;
+      stack[++top] = atof(token.c_str());
     }
-    --i;
-    
-    {
-    std::cout<<"Invalid number: "<< c <<std::endl;
-    delete[]stack;
+  }
+
+  if (top != 0)
+  {
+    std::cout << "Incorrect input" << std::endl;
+    delete[] stack;
     return 0;
-    }
+  }
 
-    stack[++top] = std::stod(token);
-  } 
-}
-
-if(top != 0)
-{
-  std::cout<< "Incorrect input" << std::endl;
-  delete[] stack;
-  return 0;
-}
-
-  std::cout<<"Result: " << stack[top] << std::endl;
+  std::cout << "Result: " << stack[top] << std::endl;
 
   delete[] stack;
   return 0;
-
 }
+
