12f1507b12d6280a9c8c164e8cd7051045974e3e
EzizBallyev <ballyev.2801@gmail.com>
2024-12-14 19:27:31 +0000

    Solved problems

---
+++ Assignment3/Desepticons.cpp
@@ -0,0 +1,30 @@
+
+#include "Desepticons.h"
+
+Desepticons::Desepticons(uint health, uint armor, uint weapon, uint power, uint dangerLevel, uint numberOfModel, uint percentOfRecharge)
+    : Transformers(health, armor, weapon, power), _dangerLevel(dangerLevel), _numberOfModel(), _percentOfRecharge() {}
+
+void Desepticons::setDangerLevel(uint dangerLevel) {
+    _dangerLevel = dangerLevel;
+}
+uint Desepticons::getDangerLevel() const {
+    return _dangerLevel;
+}
+
+void Desepticons::setNumberOfModel(uint numberOfModel) {
+    _numberOfModel = numberOfModel;
+}
+uint Desepticons::getNumberOfModel() const {
+    return _numberOfModel;
+}
+
+void Desepticons::setPercentOfRecharge(uint percentOfRecharge) {
+    _percentOfRecharge = percentOfRecharge;
+}
+uint Desepticons::getPercentOfRecharge() const {
+    return _percentOfRecharge;
+}
+
+bool Desepticons::protect() {
+    return _health > 50; 
+}
---
+++ Assignment3/Desepticons.h
@@ -0,0 +1,28 @@
+#ifndef CLASS_DECEPTICONS_H
+#define CLASS_DECEPTICONS_H
+
+#include "Transformers.h"
+#include <string>
+
+class Desepticons : public Transformers {
+public:
+    Desepticons(uint health = 90, uint armor = 55, uint weapon = 15, uint power = 80, uint dangerLevel = 5, uint numberOfModel = 1, uint percentOfRecharge = 50);
+    bool transform();
+
+    void setNumberOfModel(uint numberOfModel);
+    uint getNumberOfModel() const;
+
+    void setDangerLevel(uint dangerLevel);
+    uint getDangerLevel() const;
+
+    void setPercentOfRecharge(uint percentOfRecharge);
+    uint getPercentOfRecharge() const;
+
+    bool protect();
+private:
+    uint _dangerLevel;
+    uint _numberOfModel;
+    uint _percentOfRecharge;
+};
+
+#endif
