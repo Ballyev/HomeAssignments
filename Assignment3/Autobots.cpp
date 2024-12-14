0ec17ff2ca8750f9326526e7f43994ef0416fc20
EzizBallyev <ballyev.2801@gmail.com>
2024-12-14 21:22:36 +0000

    Added new files

---
+++ Assignment3/Autobots.cpp
@@ -0,0 +1,29 @@
+#include "Autobots.h"
+
+Autobots::Autobots(uint health, uint armor, uint weapon, uint power, const std::string& weaponType, uint skillLevel, uint sizeOfInventory)
+    : Transformers(health, armor, weapon, power), _weaponType(weaponType), _skillLevel(), _sizeOfInventory() {}
+
+void Autobots::setWeaponType(const std::string& weaponType) {
+    _weaponType = weaponType;
+}
+std::string Autobots::getWeaponType() const {
+    return _weaponType;
+}
+
+void Autobots::setSkillLevel(uint skillLevel) {
+    _skillLevel = skillLevel;
+}
+uint Autobots::getSkillLevel() const {
+    return _skillLevel;
+}
+
+void Autobots::setSizeOfInventory(uint sizeOfInventory) {
+    _sizeOfInventory = sizeOfInventory;
+}
+uint Autobots::getSizeOfInventory() const {
+    return _sizeOfInventory;
+}
+
+bool Autobots::protect() {
+    return _power > 50; 
+}
---
+++ Assignment3/Autobots.h
@@ -0,0 +1,28 @@
+#ifndef CLASS_AUTOBOTS_H
+#define CLASS_AUTOBOTS_H
+
+#include <string>
+#include "Transformers.h"
+
+class Autobots : public Transformers {
+public:
+    Autobots(uint health = 100, uint armor = 50, uint weapon = 10, uint power = 75, const std::string& weaponType = "Blaster", uint skillLevel = 3, uint sizeOfInventory = 10);
+    bool transform();
+
+    void setWeaponType(const std::string& weaponType);
+    std::string getWeaponType() const;
+    
+    void setSkillLevel(uint skillLevel);
+    uint getSkillLevel() const;
+
+    void setSizeOfInventory(uint sizeOfInventory);
+    uint getSizeOfInventory() const;
+
+    bool protect();
+private:
+    std::string _weaponType;
+    uint _skillLevel;
+    uint _sizeOfInventory;
+};
+
+#endif
