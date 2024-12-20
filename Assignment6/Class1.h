190282babf9bfb4a2377567f0df62d7fd89caaaa
EzizBallyev <ballyev.2801@gmail.com>
2024-12-20 13:34:23 +0000

    Added neccessery files

---
+++ Assignment6/Class1.h
@@ -0,0 +1,15 @@
+
+#ifndef CLASS1_H
+#define CLASS1_H
+
+#include <vector>
+
+class Class1 {
+public:
+    static bool bar(int number, const std::vector<float>& vector);
+    static int c_1_1();
+    static float c_1_2();
+    static void c_1_3();
+};
+
+#endif
---
+++ Assignment6/Class3.h
@@ -0,0 +1,15 @@
+
+#ifndef CLASS3_H
+#define CLASS3_H
+
+#include <vector>
+
+class Class3 {
+public:
+    static bool bar(int number, const std::vector<float>& vector);
+    static int c_3_1();
+    static float c_3_2();
+    static void c_3_3();
+};
+
+#endif
